55ff2a3a33046841abf1373a629b42bc754c34fb
jugesuke <103935395+jugesuke@users.noreply.github.com>
2023-05-29 15:49:17 +0900

    [add] エントリ作成用のメソッドを追加

--- app/infrastructure/userinfo.go
+++ app/infrastructure/userinfo.go
@@ -21,7 +21,11 @@ func (ur *userinfoRepositoryImpl) SelectUserinfoByUserID(userID string) (*entity
 	{
 		err := ur.db.Get(
 			profile,
-			"SELECT user_id, header_image, bio FROM user_profile WHERE user_id = ? LIMIT 1;",
+			"SELECT UP.user_id, UP.header_image, UP.bio, U.display_name, U.icon "+
+				"FROM user_profile AS UP "+
+				"INNER JOIN users AS U "+
+				"ON UP.user_id = U.id "+
+				"WHERE UP.user_id = ? LIMIT 1;",
 			userID)
 		if err != nil {
 			return nil, err
@@ -29,11 +33,15 @@ func (ur *userinfoRepositoryImpl) SelectUserinfoByUserID(userID string) (*entity
 	}
 
 	// select Groups
-	groups := new([]*entity.Group)
+	groups := new([]*entity.GroupMember)
 	{
 		err := ur.db.Select(
 			groups,
-			"SELECT id, user_id, role, status FROM groups WHERE user_id = ?;",
+			"SELECT GM.group_id, G.name, GM.user_id, GM.role, GM.status "+
+				"FROM group_member AS GM "+
+				"INNER JOIN groups AS G "+
+				"ON GM.group_id = G.id "+
+				"WHERE user_id = ?;",
 			userID)
 		if err != nil {
 			return nil, err
@@ -57,7 +65,7 @@ func (ur *userinfoRepositoryImpl) SelectUserinfoByUserID(userID string) (*entity
 	{
 		err := ur.db.Select(
 			sns,
-			"SELECT user_id, sns Where user_id = ? FROM sns;",
+			"SELECT user_id, sns FROM sns WHERE user_id = ?;",
 			userID)
 		if err != nil {
 			return nil, err
@@ -65,9 +73,66 @@ func (ur *userinfoRepositoryImpl) SelectUserinfoByUserID(userID string) (*entity
 	}
 
 	return &entity.Userinfo{
-		Profile: profile,
-		Groups:  groups,
-		Skills:  skills,
-		SNS:     sns,
+		Profile:      profile,
+		JoinedGroups: groups,
+		Skills:       skills,
+		SNS:          sns,
 	}, nil
+}
+
+func (ur *userinfoRepositoryImpl) CreateNewUserinfo(userinfo *entity.Userinfo) error {
+	tx, err := ur.db.Beginx()
+	if err != nil {
+		return err
+	}
+
+	{
+		// user profile
+		_, err := tx.NamedExec(
+			"INSERT INTO user_profile (user_id, header_image, bio) VALUES (:user_id, :header_image, :bio);",
+			userinfo.Profile,
+		)
+		if err != nil {
+			tx.Rollback()
+			return err
+		}
+	}
+
+	// group member
+	for _, group := range *userinfo.JoinedGroups {
+		_, err := tx.NamedExec(
+			"INSERT INTO group_member (group_id, user_id, role, status) VALUES (:group_id, :user_id, :role, :status);",
+			group,
+		)
+		if err != nil {
+			tx.Rollback()
+			return err
+		}
+	}
+
+	// skills
+	for _, skill := range *userinfo.Skills {
+		_, err := tx.NamedExec(
+			"INSERT INTO skills (skill_name, user_id) VALUES (:skill_name, :user_id);",
+			skill,
+		)
+		if err != nil {
+			tx.Rollback()
+			return err
+		}
+	}
+
+	// sns
+	for _, sns := range *userinfo.SNS {
+		_, err := tx.NamedExec(
+			"INSERT INTO sns (user_id, sns) VALUES (:user_id, :sns);",
+			sns,
+		)
+		if err != nil {
+			tx.Rollback()
+			return err
+		}
+	}
+
+	return tx.Commit()
 }
