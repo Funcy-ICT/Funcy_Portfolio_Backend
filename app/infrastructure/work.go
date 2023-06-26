6ff150785af0aa72436e497fb410a3390b9b2570
Shingo Itoh <g2122006@fun.ac.jp>
2023-06-26 16:59:58 +0900

    [add] タグでSQLをフィルタする関数を追加

--- app/infrastructure/work.go
+++ app/infrastructure/work.go
@@ -45,11 +45,25 @@ VALUES (?,?,?,?,?,?,?,?)`,
 	return nil
 }
 
-func (ur *workRepositoryImpl) SelectWorks(numberOfWorks uint, tag string) (*[]*entity.ReadWorksList, error) {
+func (ur *workRepositoryImpl) SelectWorks(numberOfWorks uint) (*[]*entity.ReadWorksList, error) {
 	works := new([]*entity.ReadWorksList)
 	err := ur.db.Select(
 		works,
 		"SELECT works.id, works.title, works.description, works.thumbnail, users.icon FROM works INNER JOIN work_images ON works.id = work_images.work_id INNER JOIN users ON works.user_id = users.id ORDER BY works.created_at DESC LIMIT ?",
+		numberOfWorks)
+	if err != nil {
+		return nil, err
+	}
+
+	return works, nil
+}
+
+func (ur *workRepositoryImpl) SelectWorksByTag(numberOfWorks uint, tag string) (*[]*entity.ReadWorksList, error) {
+	works := new([]*entity.ReadWorksList)
+	err := ur.db.Select(
+		works,
+		"SELECT works.id, works.title, works.description, works.thumbnail, users.icon FROM works INNER JOIN work_images ON works.id = work_images.work_id INNER JOIN work_tags ON works.id = work_tags.work_id INNER JOIN users ON works.user_id = users.id WHERE work_tags.tag=? ORDER BY works.created_at DESC LIMIT ?",
+		tag,
 		numberOfWorks)
 	if err != nil {
 		return nil, err
