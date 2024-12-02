a94b7cf232845ea25d622a685015a5f46983279a
JustHiro55 <hagi.hiro9829@gmail.com>
2024-12-02 17:09:21 +0900

    fix: rename text column to content in comment table migration file

--- db/migration/sql/20240701062632_create_comments_table.up.sql
+++ db/migration/sql/20240701062632_create_comments_table.up.sql
@@ -2,8 +2,9 @@ CREATE TABLE `comment` (
   `id` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'コメントID',
   `user_id` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'ユーザーID',
   `works_id` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '作品ID',
-  `text` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '本文',
+  `content` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '本文',
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '作成日時',
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新日時',
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_comment_works` FOREIGN KEY (`works_id`) REFERENCES `works` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
