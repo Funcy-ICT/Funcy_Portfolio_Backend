ce25f2dc6f0cad745acc47526894c8d054354df8
static-fuji <jyun1228haru@icloud.com>
2025-12-15 17:44:23 +0900

    [add] コメント削除をauth必須に変更

--- app/interfaces/server.go
+++ app/interfaces/server.go
@@ -109,12 +109,12 @@ func (s *Server) Route() {
 		// コメント関連のエンドポイント
 		mux.Route("/comment", func(r chi.Router) {
 			r.Post("/comment", commentHandler.CreateComment)
+			r.Delete("/{commentID}", commentHandler.DeleteComment)
 		})
 	})
 
 	// コメント関連のエンドポイント
 	s.Router.Get("/comment/{worksID}", commentHandler.GetComment)
-	s.Router.Delete("/comment/{commentID}", commentHandler.DeleteComment)
 
 	// no auth
 	s.Router.Get("/work/{workID}", workHandler.ReadWork)
